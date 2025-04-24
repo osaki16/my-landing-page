90281d2373917ec7bbfdb972b6d9e85739640017
osaki16 <osakipop@gmail.com>
2025-04-24 22:04:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript for basic interactivity
+document.querySelector('.find-tasks').addEventListener('click', () => {
+    alert('Redirecting to tasks page...');
+});
+
+document.querySelector('.become-creator').addEventListener('click', () => {
+    alert('Redirecting to creator signup page...');
+});
